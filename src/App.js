aba45e0171deafec06b9f199901e31fd3ab91c3b
khurram-syed <khurram245@yahoo.com>
2020-03-22 20:39:52 +0000

    KSyed: Initial Commit before form-hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom'
+import UsersPage from './pages/users/users.page';
+import NewPlace from './pages/places/NewPlace.page';
+import UpdatePlace from './pages/places/UpdatePlace.page';
+import MainNavigation from './components/shared/Navigation/MainNavigation';
+import UserPlaces from './pages/places/UserPlaces.page';
 
-function App() {
+ const App=()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+           <Router>
+             <MainNavigation />
+              <main>
+                  <Switch>
+                    <Route exact path="/" >
+                      <UsersPage />
+                  </Route>
+                  <Route exact path="/:userId/places" >
+                      <UserPlaces />
+                  </Route>
+                  <Route path="/places/new">
+                      <NewPlace />
+                  </Route>
+                  <Route path="/places/:placeId">
+                      <UpdatePlace />
+                  </Route>
+                  <Redirect to="/" />
+                  </Switch>
+              </main>
+          </Router>
   );
 }
 
